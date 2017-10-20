657695dbdeb2c2afdb20d80f7cc285762498ab2d
dshepsis <dfshepsis@gmail.com>
2017-10-20 15:10:09 -0400

    Added a debouncing function with the invariant that the returned function will not be executed more often than the given amount of time.
    For example:
    ```javascript
    let fun = throttle((name)=>console.log("Hello " + name), 1000);
    fun("John");fun("Joe");fun("Jake");fun("Jordon");
    //<1 second passes>
    //John
    ```
    
    The function also has an optional last parameter, which, when set to a
    truthy value, will make the last call to the function buffered (with its
    corresponding arguments). The last call is executed immediately when the
    timeout expires:
    
    ```javascript
    let fun = throttle((name)=>console.log("Hello " + name), 1000, true);
    fun("John");fun("Joe");fun("Jake");fun("Jordon");
    //<1 second passes>
    //John
    //Jordon
    ```
    
    The buffered call also refreshes the timer which blocks the call, which
    prevents breaking of the throttle invariant.

---
+++ JavaScript/throttle.js
@@ -0,0 +1,34 @@
+/* Returns a function which will execute the parameter function fn, and prevent
+ * re-executing the function for the next timeBetweenCalls milliseconds.
+ *
+ * It is essentially debounce-like function which doesn't refresh the blocking
+ * timer on each callback. Instead, the function is simply blocked for the
+ * specified ammount of time, regardless of the number of calls made during that
+ * time.
+ *
+ * If queue1Call is set to a truthy value, and the returned function is
+ * called while blocked, a call to the function is queued. That is, it will be
+ * called immediately when the initial timer expires (timeBetweenCalls
+ * milliseconds after the initial blocking call). Regardless of the amount of
+ * calls blocked, only 1 call to fn is queued. The queued call also refreshes
+ * the blocking timer, preserving the rate-limitting property. */
+function throttle(fn, timeBetweenCalls, queue1Call) {
+  let blocked = false, queuedArgs = null;
+  return function limitedFn() {
+    let context = this;
+    queuedArgs = (queue1Call && blocked) ? arguments : null;
+    if (blocked) return;
+    blocked = true;
+    const onTimeout = ()=>{
+      blocked = false;
+      if (queue1Call && queuedArgs !== null) {
+        limitedFn.apply(context, queuedArgs);
+      }
+    }
+    window.setTimeout(onTimeout, timeBetweenCalls);
+
+    /* Call the function with apply, to preserve
+     * context for things like prototype functions: */
+    fn.apply(context, arguments);
+  }
+}
